ac1dc7ee7df3158ef6b0f0714a411752692333e7
sheldon <liu_2648349896@126.com>
2024-03-14 23:18:26 +0800

    add Hot topic 4 - 6

---
+++ src/main/java/com/sheldon/HotTopic/maxArea_05.java
@@ -0,0 +1,37 @@
+package com.sheldon.HotTopic;
+
+/**
+ * 11. 盛最多水的容器
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ * 链接：https://leetcode-cn.com/problems/container-with-most-water
+ */
+class maxArea_05 {
+    public int maxArea(int[] height) {
+        int res = 0;
+        // 定义左右指针
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int temp = (right - left) * Math.min(height[left], height[right]);
+            // 更新最大值
+            res = Math.max(res, temp);
+            // 根据两边的高度，移动指针
+            // 因为底部在减少，所以高度要增加
+            // 所以移动高度较小的指针
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+    
+}
---
+++ src/main/java/com/sheldon/HotTopic/moveZeroes_04.java
@@ -0,0 +1,31 @@
+package com.sheldon.HotTopic;
+
+/**
+ * 283. 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ * 链接：https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+class moveZeroes_04 {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 1) {
+            return;
+        }
+        // 非零元素存放在位置
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 如果元素不为零，就移到零的位置
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // 将非零之后的元素，全部为零
+        for (int i = index + 1; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
