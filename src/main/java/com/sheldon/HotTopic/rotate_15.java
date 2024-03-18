4b2dd248505533671ab85adf5d6235463585d096
sheldon <liu_2648349896@126.com>
2024-03-18 11:25:35 +0800

    add Hot topic 13 - 15

---
+++ src/main/java/com/sheldon/HotTopic/rotate_15.java
@@ -0,0 +1,13 @@
+package com.sheldon.HotTopic;
+
+class rotate_15 {
+    public void rotate(int[] nums, int k) {
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[(i + k) % nums.length] = nums[i];
+        }
+        System.arraycopy(result, 0, nums, 0, nums.length);
+    }
+}
